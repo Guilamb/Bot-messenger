0a4a42bef2d96cdf7be4618166e566918292e412
Guilamb <guilhane@live.fr>
2018-08-03 15:21:35 +0200

    hello world

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ app.use(bodyParser.urlencoded({extended: false})) ;
 app.use(bodyParser.json()) ;
 // Index route
 app.get('/', function (req, res) {
-res.send('Hello world, je suis un chat bot’) ; }) ;
+res.send('Hello world, je suis un chat bot'); 
+});
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-if (req.query['hub.verify_token'] === 'password_facile_a_retenir’) {res.send(req.query['hub.challenge']) ;}
-res.send(‘erreur, mauvais token') ;}) ;
+if (req.query['hub.verify_token'] === 'password_facile_a_retenir') {res.send(req.query['hub.challenge']) ;}
+res.send('erreur, mauvais token') ;}) ;
 // Spin up the server
 app.listen(app.get('port'), function() {
 console.log('running on port', app.get('port')) ;
