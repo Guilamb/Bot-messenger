7f24fdcd4c645d6caa6e70f5c65edce7a9ab6c22
Guilamb <guilhane@live.fr>
2018-08-04 13:41:50 +0200

    just do it

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ app.use(bodyParser.urlencoded({extended: false})) ;
 app.use(bodyParser.json()) ;
 // Index route
 app.get('/', function (req, res) {
-res.send("Hello world, je suis un chat bot"); 
+res.send(VERIFY_TOKEN); 
 });
 // for Facebook verification
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 app.get('/webhook/', function (req, res) {
+	const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 	const VERIFY_TOKEN = "<EAAVX3oiaB0QBALDB2uGwOUc0qCQGpHOtGSjNndloXWc3j1iGa97EtYUAU7tuSO2qaX5bV9wXxZBuQNuQXBZCw0Hhy4rZAf3TtuNPwcxavpXeMFNpZCJnfrY5UZAIudqdglA67NPeQJKam9WH6OudIcCYH9AAml4GZBjwhj9LMDMwZDZD>";
 if (req.query['hub.verify_token'] === "202224") {res.send(req.query['hub.challenge']) ;}
 res.send('erreur, mauvais token') ;}) ;
