65dc125658759dc83fb62dee7abe5ee06dedd196
Guilamb <guilhane@live.fr>
2018-08-04 13:43:20 +0200

    just do it

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ app.set('port', (process.env.PORT || 5000)) ;
 app.use(bodyParser.urlencoded({extended: false})) ;
 // Process application/json
 app.use(bodyParser.json()) ;
-// Index route
-app.get('/', function (req, res) {
-res.send(VERIFY_TOKEN); 
-});
 // for Facebook verification
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 app.get('/webhook/', function (req, res) {
@@ -18,6 +14,11 @@ app.get('/webhook/', function (req, res) {
 	const VERIFY_TOKEN = "<EAAVX3oiaB0QBALDB2uGwOUc0qCQGpHOtGSjNndloXWc3j1iGa97EtYUAU7tuSO2qaX5bV9wXxZBuQNuQXBZCw0Hhy4rZAf3TtuNPwcxavpXeMFNpZCJnfrY5UZAIudqdglA67NPeQJKam9WH6OudIcCYH9AAml4GZBjwhj9LMDMwZDZD>";
 if (req.query['hub.verify_token'] === "202224") {res.send(req.query['hub.challenge']) ;}
 res.send('erreur, mauvais token') ;}) ;
+// Index route
+app.get('/', function (req, res) {
+res.send("Hello world, je suis un chat bot"); 
+});
+
 // Spin up the server
 app.listen(app.get('port'), function() {
 console.log('running on port', app.get('port')) ;
