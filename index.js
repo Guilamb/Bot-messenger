51626ee8c3af30f8f887072763b2372e365deeca
Guilamb <guilhane@live.fr>
2018-08-06 14:18:26 +0200

    make it better

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 var express = require('express') ;
 var bodyParser = require('body-parser') ;
 var request = require('request') ;
-const http = require('http'),
 var app = express() ;
 app.set('port', (process.env.PORT || 5000)) ;
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false})) ;
 // Process application/json
 app.use(bodyParser.json()) ;
-// for Facebook verification
-const VALIDATION_TOKEN = "mdp";
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
-});
 // Index route
 app.get('/', function (req, res) {
-res.send("Hello world, je suis un chat bot"); 
-});
-
+res.send('Hello world, je suis un chat bot') ;
+}) ;
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+if (req.query['hub.verify_token'] === 'password_facile_a_retenir') {
+res.send(req.query['hub.challenge']) ;
+}
+res.send('erreur, mauvais token') ;
+}) ;
 // Spin up the server
 app.listen(app.get('port'), function() {
 console.log('running on port', app.get('port')) ;
