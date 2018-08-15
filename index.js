6565247b0d96dac970413a46c591bd8e6b93497e
Guilamb <guilhanebourgin@gmail.com>
2018-08-15 22:11:41 +0200

    make it better

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send('Hello world, je suis un chat bot')
+	res.send(req.query['hub.challenge'])
 })
 
 // Facebook verification
